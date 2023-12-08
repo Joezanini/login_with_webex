614970070da6bc2ccc6ea160b27722942e660d44
Ashton Jordan <ashton.jordan@gmail.com>
2023-12-08 12:49:21 -0600

    removed testing integration clientID/secretID

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 
 class SETTINGS:
     # Enter your clientID/secretID that you received from developer.webex.com when you created your integration
-    clientID = "Cc835750c4c27674c9670f34b452ec3d72a38bd3262f41a0c9fd3bfad72ab28a1"
-    secretID = "74a4dc1a6cdd2e057b7fc8bb17ba137503976bdfe9c12b8e2014d6b5bb119cbb"
+    clientID = "<enter clientID>"
+    secretID = "<enter secretId>"
     # Enter your redirectURL for where users should be redirected to after successful Webex Authentication
     redirectURI = "http://localhost:8501/landing"
     # No CHANGES NEEDED BELOW
